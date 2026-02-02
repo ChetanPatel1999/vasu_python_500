02d2ce4ed142ead152f4f6eaa4c4124e5dc1f68a
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2026-02-02 18:05:22 +0530

    01/12

---
+++ 30_addition.py
@@ -0,0 +1,22 @@
+#this is addition program
+'single line comment'
+"this is also single line comment"
+a=12    # here a is variable which store 12
+b=70
+c=a+b
+# c=a-b
+print("addition = ",c)
+# print("subtraction = ",c)
+# single line comment
+
+
+"""this is 
+multiline 
+comment"""
+
+'''
+you can also
+use triple single
+quotes for 
+multiline comment
+'''
