947ae9278c7f417ede58ffcee581cbac2f642bac
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-04 19:22:36 +0530

    02/02

---
+++ 31_airtmatic_operators.py
@@ -0,0 +1,13 @@
+# example of airthmatic operator
+a=4
+b=3
+#c=a+b
+#c=a-b
+#c=a*b
+#c=a/b
+# c=a//b
+# c=a%b
+c=a**b
+print("res = ",c)
+
+
---
+++ 32_relational_operators.py
@@ -0,0 +1,5 @@
+#example of comparision operator
+a=12
+b=78
+c=a<b
+print("res = ",c)
