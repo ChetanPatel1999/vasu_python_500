b6b4621258b2afb9171c897554cb187f5882e02b
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-05 18:08:24 +0530

    02/02

---
+++ 33_compound_assignment.py
@@ -0,0 +1,17 @@
+# example of compound assignment
+a=12
+b=5
+print("a = ",a)
+print("b = ",b)
+
+# a+=b   # a=a+b
+# a-=b
+# a+=10
+# b*=a
+# b*=10
+# a+=1
+# b**=3
+a%=b
+
+print("a = ",a)
+print("b = ",b)
