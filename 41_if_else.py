d9aad13dc358f85f90539c71efcf38360964b4b3
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-10 18:00:42 +0530

    09/02

---
+++ 41_if_else.py
@@ -0,0 +1,7 @@
+# Write a program to accept two number from user and display greatest number. 
+a=int(input("enter a num : "))
+b=int(input("enter b num : "))
+if a>b:
+    print("greatest num = ",a)
+else:
+    print("greatest num = ",b)    
---
+++ 43_if_else.py
@@ -0,0 +1,6 @@
+#  Write a program to check given char is vowel or consonant. 
+alpha=input("enter a alpha : ")#e
+if alpha in "aeiouAEIOU":
+    print("alpha is vovel")
+else:
+    print("alpha is consonent")    
