1580c9a70d7a09570f83d0e12137b500c295cefa
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-03-14 18:08:58 +0530

    14/03

---
+++ 145_string.py
@@ -0,0 +1,16 @@
+# we can iterate string charcter in three way
+s="indore is clean city"
+
+i=0
+while i<len(s):
+    print(s[i])
+    i+=1
+
+
+# for char in s:
+#     print(char)
+
+# for i in range(len(s)): # 1
+#     print(s[i])
+
+
