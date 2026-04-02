20119ac8e207b635c33783787e7144e4f457b302
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-04-02 18:02:18 +0530

    01/04

---
+++ 199_set.py
@@ -0,0 +1,11 @@
+#set in python
+
+s1={34,56,22,89,66}
+print(s1)
+
+# s1.add(344)
+
+for num in s1:
+    print(num)
+    
+print(s1)
---
+++ 200_set.py
@@ -0,0 +1,15 @@
+s1={12,34,56,78,90}
+
+# s1.remove(56)
+# s1.remove(5) # its provide run time error if pass element not exist in set
+
+# s1.discard(34)
+# s1.discard(4) # its not raise error if element not exist in set
+
+# delete= s1.pop()
+# print(delete)
+
+# s1.clear()
+del s1
+
+print(s1)
